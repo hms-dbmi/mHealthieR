e7d5d6a3e6644949d7223256be28f405344d54f2
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-07-12 11:43:10 -0400

    One passing test, one failing, which I do not understand

--- tests/testthat/test_data_handling.R
+++ tests/testthat/test_data_handling.R
@@ -1,6 +1,11 @@
 context("data_handling")
 
 test_that("check_format warns about wide tables", {
-  wide_table <- matrix(c("id-a", 1, "id-b", 4), ncol=2, byrow=TRUE)
+  wide_table <- as.table(matrix(c("id-a", 1, 2, 3, "id-b", 4, 5, 6), ncol=4, byrow=TRUE))
   expect_warning(check_format(wide_table))
+})
+
+test_that("check_format gets long tables", {
+  long_table <- as.table(matrix(c("id-a", as.POSIXct('2001-01-01'), 42), ncol=3, byrow=TRUE))
+  expect_equal(check_format(long_table), "long")
 })
